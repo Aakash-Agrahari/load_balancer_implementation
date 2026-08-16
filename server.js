523813ca12c544daff3786f24b4e332871b85d94
Aakash Agrahari <aakashagrahari45@gmail.com>
2026-08-16 15:41:14 +0530

    added first server

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const PORT = 3001;
+
+app.get("/", (req, res) => {
+    res.send("Hello from Server 1");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server 1 running on port ${PORT}`);
+});
