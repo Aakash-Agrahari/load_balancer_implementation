a0e94e913d963f1ea1a0561a1995a1be92065b8b
Aakash Agrahari <aakashagrahari45@gmail.com>
2026-08-16 22:32:33 +0530

    minimal changes in the server

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,12 @@ import express from "express";
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.get('/', (req,res) => {
+app.get("/", (req,res) => {
     res.send(`Hello from Server ${PORT}`)
 });
 
-app.get('/health', (res,req) => {
-    res.statusCode(200).json({
+app.get("/health", (req,res) => {
+    res.status(200).json({
         status: "UP",
         port: PORT
     });
