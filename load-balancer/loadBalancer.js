c13aa8569127a0168b435e3a5f5bc084e3f10e6d
Aakash Agrahari <aakashagrahari45@gmail.com>
2026-08-16 22:08:05 +0530

    added round robin LB logic

--- load-balancer/loadBalancer.js
+++ load-balancer/loadBalancer.js
@@ -2,38 +2,74 @@ import http from "http";
 
 const PORT = 3000;
 
- const TARGET_HOST = "localhost";
- const TARGET_PORT = 3001;
+const servers = [
+    {
+        host: "localhost",
+        port: 3001
+    },
+    {
+        host: "localhost",
+        port: 3002
+    },
+    {
+        host: "localhost",
+        port: 3003
+    }
+];
 
- const server = http.createServer((req, res) => {
+let currentServerIndex = 0;
+
+const server = http.createServer((req, res) => {
+
+    const targetServer = servers[currentServerIndex];
+
+    currentServerIndex =
+        (currentServerIndex + 1) % servers.length;
+
+    console.log(
+        `Forwarding ${req.method} ${req.url} → localhost:${targetServer.port}`
+    );
 
     const options = {
-        hostname: TARGET_HOST,
-        port: TARGET_PORT,
+        hostname: targetServer.host,
+        port: targetServer.port,
         path: req.url,
         method: req.method,
         headers: req.headers
     };
 
-    const proxyRequest = http.request(options, (proxyResponse) => {
-        res.writeHead(
-            proxyResponse.statusCode,
-            proxyResponse.headers
-        );
-        proxyResponse.pipe(res);
-    });
+    const proxyRequest = http.request(
+        options,
+        (proxyResponse) => {
+
+            res.writeHead(
+                proxyResponse.statusCode,
+                proxyResponse.headers
+            );
+
+            proxyResponse.pipe(res);
+        }
+    );
 
     proxyRequest.on("error", (error) => {
-        console.error("Proxy error:", error.message);
+
+        console.error(
+            `Server ${targetServer.port} error:`,
+            error.message
+        );
 
         res.writeHead(502, {
             "Content-Type": "text/plain"
         });
+
         res.end("Bad Gateway");
     });
-    req.pipe(proxyRequest);
- });
 
- server.listen(PORT, () => {
-    console.log(`Load balancer is running on port ${PORT}`);
- })
+    req.pipe(proxyRequest);
+});
+
+server.listen(PORT, () => {
+    console.log(
+        `Load balancer is running on port ${PORT}`
+    );
+});
